55d32eefb331f99ab90dd25b11bf355206cc32a5
sally11131114 <sally11131114@gmail.com>
2022-03-01 19:52:34 +0800

    delete large file

--- hw1/pytorch101.py
+++ hw1/pytorch101.py
@@ -651,6 +651,7 @@ def mm_on_gpu(x: Tensor, w: Tensor) -> Tensor:
     #                      TODO: Implement this function                     #
     ##########################################################################
     # Replace "pass" statement with your code
+    torch.backends.cuda.matmul.allow_tf32 = False
     x=x.cuda()
     w=w.cuda()
     y=x.mm(w).cpu()
